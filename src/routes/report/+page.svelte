3e2dac75a7e885790b13b6624b3b06a8e46971b0
Marina19761107 <marinasal1107@gmail.com>
2026-05-11 19:25:33 +0100

    has been created 2 files - card.svelte and report/+page svelte

---
+++ src/routes/report/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { subTitle } from '$lib/runes.svelte';
+	import Card from '$lib/ui/Card.svelte';
+	import PlaceMarkList from '$lib/ui/PlaceMarkist.svelte';
+
+	subTitle.text = 'Placemark to date';
+</script>
+
+<Card title="PlaceMark">
+	<PlaceMarkList />
+</Card>
