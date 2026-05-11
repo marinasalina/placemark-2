ba7804337b87589a72ed4635316998e2f6b4f3a4
Marina19761107 <marinasal1107@gmail.com>
2026-05-11 19:29:35 +0100

    Pla<script lang=ts>

--- src/routes/PlaceMark/PlaceMarkForm.svelte
+++ src/routes/PlaceMark/PlaceMarkForm.svelte
@@ -3,18 +3,18 @@
 
 <div>
 	<div class="field">
-		<label class="label" for="amount">Enter PlaceMark name:</label>
-		<input class="input" id="amount" name="amount" type="number" />
+		<label class="label" for="PlaceMark">Enter PlaceMark name:</label>
+		<input class="input" id="PlaceMark" name="PlaceMark" type="string" />
 	</div>
 	<div class="field">
 		<div class="control">
-			<label class="label" for="amount">Select Rating:</label>
+			<label class="label" for="PlaceMark">Select Rating:</label>
 			<input class="radio" type="radio" /> Recommend
 			<input class="radio" type="radio" /> Do not recommensd
 		</div>
 	</div>
 	<div class="field">
-		<label class="label" for="amount">Select Category:</label>
+		<label class="label" for="PlaceMark">Select Category:</label>
 		<div class="select">
 			<select>
 				<option>Dublin</option>
