659b85f468781c0431a8e904009d50183ed53fb3
Marina19761107 <marinasal1107@gmail.com>
2026-05-12 18:35:52 +0100

    PlaceMarkForm has been updated

--- src/routes/PlaceMark/PlaceMarkForm.svelte
+++ src/routes/PlaceMark/PlaceMarkForm.svelte
@@ -1,31 +1,50 @@
 <script lang="ts">
+	import PlaceMarkList from '$lib/ui/PlaceMarkist.svelte';
+
+	let PlaceMarkName = $state('');
+	let PlaceMarkDescription = $state('');
+	let selectedCategory = $state('City');
+	let selectedMethod = $state('recommend');
+	let lat = $state(0);
+	let lng = $state(0);
+
+	async function addPlacemark() {
+		console.log('Placemark added:');
+		console.log('Name:', PlaceMarkName);
+		console.log('Description:', PlaceMarkDescription);
+		console.log('Category:', selectedCategory);
+		console.log('Rating:', selectedMethod);
+		console.log('Lat:', lat, 'Lng:', lng);
+	}
 </script>
 
 <div>
 	<div class="field">
-		<label class="label" for="PlaceMark">Enter PlaceMark name:</label>
-		<input class="input" id="PlaceMark" name="PlaceMark" type="string" />
+		<label class="label">Enter Placemark name:</label>
+		<input class="input" bind:value={PlaceMarkName} type="text" />
 	</div>
+
 	<div class="field">
-		<div class="control">
-			<label class="label" for="PlaceMark">Select Rating:</label>
-			<input class="radio" type="radio" /> Recommend
-			<input class="radio" type="radio" /> Do not recommensd
-		</div>
+		<label class="label">Enter Placemark description:</label>
+		<input class="input" bind:value={PlaceMarkDescription} type="text" />
 	</div>
+
 	<div class="field">
-		<label class="label" for="PlaceMark">Select Category:</label>
-		<div class="select">
-			<select>
-				<option>Dublin</option>
-				<option>Monument</option>
-				<option>Library</option>
-			</select>
-		</div>
+		<label class="label">Select Rating:</label>
+		<input type="radio" bind:group={selectedMethod} value="recommend" /> Recommend
+		<input type="radio" bind:group={selectedMethod} value="do not recommend" /> Do not recommend
 	</div>
+
 	<div class="field">
-		<div class="control">
-			<button class="button is-success is-fullwidth">Add placeMark</button>
-		</div>
+		<label class="label">Select Category:</label>
+		<select bind:value={selectedCategory}>
+			<option>City</option>
+			<option>Monument</option>
+			<option>Library</option>
+		</select>
+	</div>
+
+	<div class="field">
+		<button class="button is-success is-fullwidth" onclick={addPlacemark}> Add Placemark </button>
 	</div>
 </div>
