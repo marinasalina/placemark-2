b49df58ddaaa2e46a5186d32512efdf2a7faf47c
Marina19761107 <marinasal1107@gmail.com>
2026-05-14 19:11:45 +0100

    PlaceMarkFOrs has been updated

--- placemark-svelte/src/routes/PlaceMark/PlaceMarkForm.svelte
+++ placemark-svelte/src/routes/PlaceMark/PlaceMarkForm.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import Coordinates from '$lib/ui/Coordinates.svelte';
-	import { placemarkService } from '$lib/services/placemark-service';
+	import { placemarkService } from '$lib/services/PlaceMark-service';
 
 	let { placemarks } = $props();
+
 	let PlaceMarkName = $state('');
 	let PlaceMarkDescription = $state('');
 	let selectedCategory = $state('City');
@@ -11,25 +12,19 @@
 	let lng = $state(0);
 
 	async function addPlacemark() {
-		const success = await placemarkService.addPlacemark({
+		const placemark = {
 			name: PlaceMarkName,
 			description: PlaceMarkDescription,
 			category: selectedCategory,
 			rating: selectedMethod,
 			lat,
 			lng
-		});
+		};
+
+		const success = await placemarkService.addPlacemark(placemark);
 
 		if (success) {
-			placemarks.push({
-				name: PlaceMarkName,
-				description: PlaceMarkDescription,
-				category: selectedCategory,
-				rating: selectedMethod,
-				lat,
-				lng
-			});
-
+			placemarks.push(placemark);
 			PlaceMarkName = '';
 			PlaceMarkDescription = '';
 			lat = 0;
@@ -37,34 +32,3 @@
 		}
 	}
 </script>
-
-<div>
-	<div class="field">
-		<label class="label">Enter Placemark name:</label>
-		<input class="input" bind:value={PlaceMarkName} type="text" />
-	</div>
-
-	<div class="field">
-		<label class="label">Enter Placemark description:</label>
-		<input class="input" bind:value={PlaceMarkDescription} type="text" />
-	</div>
-
-	<div class="field">
-		<label class="label">Select Rating:</label>
-		<input type="radio" bind:group={selectedMethod} value="recommend" /> Recommend
-		<input type="radio" bind:group={selectedMethod} value="do not recommend" /> Do not recommend
-	</div>
-
-	<div class="field">
-		<label class="label">Select Category:</label>
-		<select bind:value={selectedCategory}>
-			<option>City</option>
-			<option>Monument</option>
-			<option>Library</option>
-		</select>
-	</div>
-
-	<Coordinates bind:lat bind:lng />
-
-	<button class="button is-success is-fullwidth" onclick={addPlacemark}> Add Placemark </button>
-</div>
