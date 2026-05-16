bbd52df2a864d146e7bc962b1b5b10522bf24d85
Marina19761107 <marinasal1107@gmail.com>
2026-05-16 22:58:36 +0100

    Add SSR MongoDB server modes and data connection for User

---
+++ placemark-svelte/src/lib/server/auth.ts
@@ -0,0 +1,50 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { User } from '$lib/server/models/user';
+
+const JWT_SECRET = 'secret';
+
+export async function createUser(userData: {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+}) {
+	const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+	const user = new User({
+		firstName: userData.firstName,
+		lastName: userData.lastName,
+		email: userData.email.trim().toLowerCase(),
+		password: hashedPassword
+	});
+
+	await user.save();
+
+	return user;
+}
+
+export async function authenticateUser(email: string, password: string) {
+	const user = await User.findOne({
+		email: email.trim().toLowerCase()
+	});
+
+	if (!user) return null;
+
+	const validPassword = await bcrypt.compare(password, user.password);
+
+	if (!validPassword) return null;
+
+	const token = jwt.sign(
+		{
+			id: user._id
+		},
+		JWT_SECRET
+	);
+
+	return {
+		name: `${user.firstName} ${user.lastName}`,
+		_id: user._id.toString(),
+		token
+	};
+}
