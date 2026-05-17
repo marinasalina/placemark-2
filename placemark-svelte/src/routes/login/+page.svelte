8ca56fbd1aa1e33c7982c1dc93dc23297e3cfc13
Marina19761107 <marinasal1107@gmail.com>
2026-05-17 11:54:14 +0100

    OAth (login via Github and Google, still not working)

--- placemark-svelte/src/routes/login/+page.svelte
+++ placemark-svelte/src/routes/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import LoginForm from './LoginForm.svelte';
+	import { signIn } from '@auth/sveltekit/client';
 </script>
 
 <section class="hero is-fullheight">
@@ -7,7 +8,20 @@
 		<div class="container">
 			<div class="column is-4 is-offset-4">
 				<h3 class="title has-text-black has-text-centered">Login to PlaceMark</h3>
+
 				<LoginForm />
+
+				<div class="has-text-centered mt-5">
+					<p class="mb-3">Or sign in with</p>
+
+					<button class="button is-danger is-fullwidth mb-3" on:click={() => signIn('google')}>
+						Login with Google
+					</button>
+
+					<button class="button is-dark is-fullwidth" on:click={() => signIn('github')}>
+						Login with GitHub
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
