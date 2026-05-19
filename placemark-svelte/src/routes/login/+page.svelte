faf26e187534e300b02748cf953d2e210c52e01b
Marina19761107 <marinasal1107@gmail.com>
2026-05-19 17:13:15 +0100

    Firebase

--- placemark-svelte/src/routes/login/+page.svelte
+++ placemark-svelte/src/routes/login/+page.svelte
@@ -3,14 +3,19 @@
 	import { signIn } from '@auth/sveltekit/client';
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
+	import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 	let { data, form } = $props();
 
-	$effect(() => {
-		if (data.session) {
-			goto(resolve('/PlaceMark'));
-		}
-	});
+	async function googleLogin() {
+		await signInWithPopup(auth, new GoogleAuthProvider());
+		goto(resolve('/PlaceMark'));
+	}
+
+	async function githubLogin() {
+		await signInWithPopup(auth, new GithubAuthProvider());
+		goto(resolve('/PlaceMark'));
+	}
 </script>
 
 <section class="hero is-fullheight">
@@ -24,16 +29,18 @@
 				<div class="has-text-centered mt-5">
 					<p class="mb-3">Or sign in with</p>
 
-						<button class="button is-danger is-fullwidth mb-3" onclick={() => signIn('google')}>
-							Login with Google
-						</button>
+					<button class="button is-danger is-fullwidth mb-3" onclick={googleLogin}>
+						Login with Google
+					</button>
 
-						<button class="button is-dark is-fullwidth" onclick={() => signIn('github')}>
+					<button class="button is-dark is-fullwidth" onclick={githubLogin}>
 						Login with GitHub
 					</button>
 				</div>
 			</div>
-			<a class="button is-link is-light is-fullwidth mt-3" href={resolve("/signup")}> Back to Sign Up </a>
+			<a class="button is-link is-light is-fullwidth mt-3" href={resolve('/signup')}>
+				Back to Sign Up
+			</a>
 		</div>
 	</div>
 </section>
